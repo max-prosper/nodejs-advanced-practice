6572658e0a75e196798645013b291a4c3ecc44fb
Max Prosper <maxim.yaaman@gmail.com>
2018-04-10 22:12:29 +0300

    Generating Sessions and Signatures

--- tests/header.test.js
+++ tests/header.test.js
@@ -2,23 +2,44 @@ const puppeteer = require("puppeteer");
 let browser, page;
 
 beforeEach(async () => {
-    browser = await puppeteer.launch({ headless: false });
-    page = await browser.newPage();
-    await page.goto("localhost:3000");
-})
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  await page.goto("localhost:3000");
+});
 
-afterEach( async () => {
-    await browser.close();
-})
+// afterEach(async () => {
+//   await browser.close();
+// });
 
 test("the header has the correct text", async () => {
-    const text = await page.$eval("a.brand-logo", el => el.innerHTML);
-    expect(text).toEqual("Blogster");
-})
+  const text = await page.$eval("a.brand-logo", el => el.innerHTML);
+  expect(text).toEqual("Blogster");
+});
 
 test("clicking login starts oauth flow", async () => {
-    await page.click(".right a");
-    const url = await page.url();
+  await page.click(".right a");
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+});
 
-    expect(url).toMatch(/accounts\.google\.com/);
-})
+test.only("when signed in, shows logout button", async () => {
+  const id = "5ac8f3d111d50e3d6d4eff4e";
+
+  const Buffer = require("safe-buffer").Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(
+    JSON.stringify(sessionObject)).toString("base64");
+
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign("session=" + sessionString);
+
+  await page.setCookie({ name: "session", value: sessionString });
+  await page.setCookie({ name: "session.sig", value: sig });
+  await page.goto("localhost:3000");
+});
