a303a9001be8c49b0bec7c7ba93e47bb7e719334
Max Prosper <maxim.yaaman@gmail.com>
2018-04-11 21:35:27 +0300

    User and Session factories. Jest setup file

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,7 @@
 const puppeteer = require("puppeteer");
+const sessionFactory = require("./factories/sessionFactory");
+const userFactory = require("./factories/userFactory");
+
 let browser, page;
 
 beforeEach(async () => {
@@ -7,9 +10,9 @@ beforeEach(async () => {
   await page.goto("localhost:3000");
 });
 
-// afterEach(async () => {
-//   await browser.close();
-// });
+afterEach(async () => {
+  await browser.close();
+});
 
 test("the header has the correct text", async () => {
   const text = await page.$eval("a.brand-logo", el => el.innerHTML);
@@ -22,25 +25,11 @@ test("clicking login starts oauth flow", async () => {
   expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test.only("when signed in, shows logout button", async () => {
-  const id = "5ac8f3d111d50e3d6d4eff4e";
+test("when signed in, shows logout button", async () => {
+  const user = await userFactory();  
+  const { session, sig } = sessionFactory(user);
 
-  const Buffer = require("safe-buffer").Buffer;
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  };
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
-    "base64"
-  );
-
-  const Keygrip = require("keygrip");
-  const keys = require("../config/keys");
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign("session=" + sessionString);
-
-  await page.setCookie({ name: "session", value: sessionString });
+  await page.setCookie({ name: "session", value: session });
   await page.setCookie({ name: "session.sig", value: sig });
   await page.goto("localhost:3000");
   await page.waitFor('a[href="/auth/logout"]');
