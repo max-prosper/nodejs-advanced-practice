2c42ea88a3fc14f19f9245ea97556836e04a8daa
Max Prosper <maxim.yaaman@gmail.com>
2018-04-13 22:06:20 +0300

    Custom Page implementation

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,17 +1,15 @@
-const puppeteer = require("puppeteer");
-const sessionFactory = require("./factories/sessionFactory");
-const userFactory = require("./factories/userFactory");
 
-let browser, page;
+const Page = require("./helpers/page");
+
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({ headless: false });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto("localhost:3000");
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test("the header has the correct text", async () => {
@@ -26,15 +24,7 @@ test("clicking login starts oauth flow", async () => {
 });
 
 test("when signed in, shows logout button", async () => {
-  const user = await userFactory();  
-  const { session, sig } = sessionFactory(user);
-
-  await page.setCookie({ name: "session", value: session });
-  await page.setCookie({ name: "session.sig", value: sig });
-  await page.goto("localhost:3000");
-  await page.waitFor('a[href="/auth/logout"]');
-
+  await page.login();
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
-
   expect(text).toEqual("Logout");
 });
