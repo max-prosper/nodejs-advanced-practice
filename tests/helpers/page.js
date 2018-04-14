b5a3e7a2058007fbd0bfb4356b7fb4bc25cff535
Max Prosper <maxim.yaaman@gmail.com>
2018-04-14 15:48:55 +0300

    TravisCI config

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -7,7 +7,7 @@ const fetch = global.fetch;
 class CustomPage {
   static async build() {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       args: ["--no-sandbox"]
     });
 
@@ -31,7 +31,7 @@ class CustomPage {
 
     await this.page.setCookie({ name: "session", value: session });
     await this.page.setCookie({ name: "session.sig", value: sig });
-    await this.page.goto("localhost:3000/blogs");
+    await this.page.goto("http://localhost:3000/blogs");
     await this.page.waitFor('a[href="/auth/logout"]');
   }
 
