2df32a7ccfb639ad6e1d83be3816df9ea317627b
Max Prosper <maxim.yaaman@gmail.com>
2018-04-14 14:44:20 +0300

    Tests complete

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -5,8 +5,10 @@ const userFactory = require("../factories/userFactory");
 class CustomPage {
   static async build() {
     const browser = await puppeteer.launch({
-      headless: false
+      headless: false,
+      args: ["--no-sandbox"]
     });
+    
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
 
@@ -27,8 +29,48 @@ class CustomPage {
 
     await this.page.setCookie({ name: "session", value: session });
     await this.page.setCookie({ name: "session.sig", value: sig });
-    await this.page.goto("localhost:3000");
+    await this.page.goto("localhost:3000/blogs");
     await this.page.waitFor('a[href="/auth/logout"]');
+  }
+
+  async getContentsOf(selector) {
+    return this.page.$eval(selector, el => el.innerHTML);
+  }
+  get(path) {
+    return this.page.evaluate(_path => {
+      return fetch(_path, {
+        method: "GET",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(res => res.json());
+    }, path);
+  }
+
+  post(path, data) {
+    return this.page.evaluate(
+      (_path, _data) => {
+        return fetch(_path, {
+          method: "POST",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(_data)
+        }).then(res => res.json());
+      },
+      path,
+      data
+    );
+  }
+
+  execRequests(actions) {
+    return Promise.all(
+      actions.map(({ method, path, data }) => {
+        return this[method](path, data);
+      })
+    );
   }
 }
 
